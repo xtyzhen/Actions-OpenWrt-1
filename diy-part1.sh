9136e5b8681c95635c10697066e6a9baa0153845
xtyzhen <25572441+xtyzhen@users.noreply.github.com>
2022-06-19 17:49:22 +0800

    修复源失败导致的download错误

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,3 +28,6 @@ git gperf haveged help2man intltool libc6-dev-i386 libelf-dev libglib2.0-dev lib
 libmpc-dev libmpfr-dev libncurses5-dev libncursesw5-dev libreadline-dev libssl-dev libtool lrzsz \
 mkisofs msmtp nano ninja-build p7zip p7zip-full patch pkgconf python2.7 python3 python3-pip qemu-utils \
 rsync scons squashfs-tools subversion swig texinfo uglifyjs upx-ucl unzip vim wget xmlto xxd zlib1g-dev
+wget -P ./openwrt/dl https://mirrors.bfsu.edu.cn/raspberry-pi-os/raspbian/pool/main/b/bc/bc_1.06.95.orig.tar.gz
+wget -O ./openwrt/dl/dns2tcp-0.5.2.tar.gz  https://mirrors.tuna.tsinghua.edu.cn/raspbian/raspbian/pool/main/d/dns2tcp/dns2tcp_0.5.2.orig.tar.gz 
+
